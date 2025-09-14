16c688531851e60a189c02c76c62e39033e4caf4
BernieT7 <bernie.t0610@gmail.com>
2025-09-15 02:02:58 +0800

    first commit

---
+++ indicators_generator.py
@@ -0,0 +1,72 @@
+import pandas as pd
+import numpy as np
+
+data = pd.read_csv("data.csv")
+df = data.copy()
+df= df.iloc[::5, :]
+
+df["spread"] = [float(ask[1: -1].split(", ")[0]) - float(bid[1: -1].split(", ")[0]) for bid, ask in zip(df["bid"], df["ask"])]
+df["mid price"] = [(float(ask[1: -1].split(", ")[0]) + float(bid[1: -1].split(", ")[0]))/2 for bid, ask in zip(df["bid"], df["ask"])]
+
+total_bid_qty = []
+for qty in df["bid_qty"]:
+  qty_list = qty[1: -1].split(", ")
+  temp_qty = 0
+  for i in range(len(qty_list)):
+    temp_qty += float(qty_list[i])
+  total_bid_qty.append(temp_qty)
+
+total_ask_qty = []
+for qty in df["ask_qty"]:
+  qty_list = qty[1: -1].split(", ")
+  temp_qty = 0
+  for i in range(len(qty_list)):
+    temp_qty += float(qty_list[i])
+  total_ask_qty.append(temp_qty)
+
+df["bid total qty"] = total_bid_qty
+df["ask total qty"] = total_ask_qty
+
+df["order imbalance"] = (df["bid total qty"] - df["ask total qty"])/(df["bid total qty"] + df["ask total qty"])
+
+df["delta price"] = df["close"] - df["close"].shift(1)
+df["delta volume"] = df["volume"] - df["volume"].shift(1)
+
+df["daily_ret"] = df["close"].pct_change()
+df['direction'] = np.where(df['daily_ret']>=0,1,-1)
+df['direction'][0] = 0
+df['vol_adj'] = df['volume'] * df['direction']
+df['obv'] = df['vol_adj'].cumsum()
+
+slow_window = 26
+fast_window = 12
+signal_window = 9
+df["slow_ema"] = df["close"].ewm(span=slow_window, min_periods=slow_window).mean()
+df["fast_ema"] = df["close"].ewm(span=fast_window, min_periods=fast_window).mean()
+df["macd"] = df["fast_ema"] - df["slow_ema"]
+df["signal"] = df["macd"].ewm(span=signal_window, min_periods=signal_window).mean()
+df["macd signal"] = df["macd"] - df["signal"]
+
+n = 20
+df['H-L']=abs(df['high']-df['low'])
+df['H-PC']=abs(df['high']-df['close'].shift(1))
+df['L-PC']=abs(df['low']-df['close'].shift(1))
+df['TR']=df[['H-L','H-PC','L-PC']].max(axis=1,skipna=False)
+df['ATR'] = df['TR'].rolling(n).mean()
+df['ATR'] = df['TR'].ewm(span=n,adjust=False,min_periods=n).mean()
+
+window = 14
+df["+change"] = df["high"] - df["high"].shift(1)
+df["-change"] = df["low"].shift(1) - df["low"]
+df["+DM"] = np.where((df["+change"] > df["-change"]) & (df["+change"] > 0), df["+change"], 0)
+df["-DM"] = np.where((df["-change"] > df["+change"]) & (df["-change"] > 0), df["-change"], 0)
+df["+DI14"] = 100 * (df["+DM"].ewm(com=window, min_periods=window).mean() / df["ATR"])
+df["-DI14"] = 100 * (df["-DM"].ewm(com=window, min_periods=window).mean() / df["ATR"])
+df["DX"] = 100 * (abs(df["+DI14"] - df["-DI14"]) / (df["+DI14"] + df["-DI14"]))
+df["ADX"] = df["DX"].ewm(com=window, min_periods=window).mean()
+
+df["return"] = df["close"].pct_change()
+df.dropna(inplace=True)
+
+indicators = df[["order imbalance", "macd signal", "spread", "mid price", "delta price", "delta volume", "ADX", "obv", "return"]]
+indicators.to_csv("reg_indicators.csv", index=False)
---
+++ predict.py
@@ -0,0 +1,111 @@
+import pandas as pd
+import numpy as np
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import LinearRegression
+import matplotlib.pyplot as plt
+
+dataset = pd.read_csv("reg_indicators.csv")
+y = dataset.iloc[:, -1].values
+
+X_no_scale = dataset[["order imbalance"]].values
+X_to_scale = dataset[["macd signal", "ADX", "obv", "mid price", "delta volume", "delta price", "spread"]].values
+
+split_index = int(len(y) * 0.8)
+
+X_to_scale_train = X_to_scale[:split_index]
+X_to_scale_test = X_to_scale[split_index:]
+X_no_scale_train = X_no_scale[:split_index]
+X_no_scale_test = X_no_scale[split_index:]
+y_train = y[:split_index]
+y_test  = y[split_index:]
+
+sc = StandardScaler()
+X_to_scale_train = sc.fit_transform(X_to_scale_train)
+X_to_scale_test = sc.transform(X_to_scale_test)
+
+X_train = np.hstack((X_no_scale_train, X_to_scale_train))
+X_test = np.hstack((X_no_scale_test, X_to_scale_test))
+
+regressor = LinearRegression()
+regressor.fit(X_train, y_train)
+
+y_pred = regressor.predict(X_test)
+np.set_printoptions(precision=2)
+print(np.concatenate((y_pred.reshape(len(y_pred), 1), y_test.reshape(len(y_test), 1)), axis=1))
+
+data = pd.read_csv("data.csv")
+df = data.copy()
+price_plt = df.iloc[::5, :].reset_index(drop=True)
+
+position = ""
+initial_price = 0
+end_price = 0
+pos_return = 0
+total_ret = []
+
+ATR = dataset["ATR%"]
+
+long_entries = []
+short_entries = []
+exits = []
+
+for i in range(len(y_pred)):
+    threshold = 1.5 * y_test[: i].std()
+
+    if position == "":
+        if y_pred[i] > threshold:
+            position = "long"
+            initial_price = price_plt["close"][split_index+i]
+            long_entries.append((split_index+i, initial_price))
+
+        elif y_pred[i] < -threshold:
+            position = "short"
+            initial_price = price_plt["close"][split_index+i]
+            short_entries.append((split_index+i, initial_price))
+
+    elif position == "long":
+        end_price = price_plt["close"][split_index+i]
+        pos_return = (end_price / initial_price) - 1
+        if (pos_return >= 2*ATR[split_index+i]) or (pos_return <= -1*ATR[split_index+i]) or (y_pred[i] < -threshold):
+            position = ""
+            initial_price = 0
+            total_ret.append(pos_return)
+            exits.append((split_index+i, end_price))
+
+    elif position == "short":
+        end_price = price_plt["close"][split_index+i]
+        pos_return = 1 - (end_price / initial_price)
+        if (pos_return >= 2*ATR[split_index+i]) or (pos_return <= -1*ATR[split_index+i]) or (y_pred[i] > threshold):
+            position = ""
+            initial_price = 0
+            total_ret.append(pos_return)
+            exits.append((split_index+i, end_price))
+
+ret = 1
+for r in total_ret:
+    ret *= (1+r)
+ret -= 1
+print("總報酬:", ret)
+print("Long:", len(long_entries))
+print("Short:", len(short_entries))
+print("Exit:", len(exits))
+
+print("y_pred 範圍:", y_pred.min(), y_pred.max())
+print("threshold 範例:", 1.5 * y_test[:10].std())
+plt.figure(figsize=(14,7))
+plt.plot(price_plt["close"][split_index:], label="Price", color="blue")
+
+plt.scatter([x[0] for x in long_entries], [x[1] for x in long_entries],
+            color="green", marker="^", s=80, label="Long Entry")
+
+plt.scatter([x[0] for x in short_entries], [x[1] for x in short_entries],
+            color="red", marker="v", s=80, label="Short Entry")
+
+plt.scatter([x[0] for x in exits], [x[1] for x in exits],
+            color="black", marker="x", s=80, label="Exit")
+
+plt.legend()
+plt.title("Trading Strategy Simulation")
+plt.xlabel("Index")
+plt.ylabel("Price")
+plt.show()
